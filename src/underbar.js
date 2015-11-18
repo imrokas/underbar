dbdc23a7b2ed9b0e4dc4064590bf8614217b5d2f
imrokas <rokas@imrokas.com>
2015-11-17 22:34:22 -0500

    implemented map function

--- src/underbar.js
+++ src/underbar.js
@@ -108,7 +108,6 @@
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
     var uniqueElements = [];
-    console.log(_.indexOf(uniqueElements, 3));
     _.each(array, function (element){
       if (_.indexOf(uniqueElements, element) === -1) {
         uniqueElements.push(element);
@@ -123,6 +122,11 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var results = [];
+    _.each(collection, function(value, key, collection) {
+      results.push(iterator(value, key, collection));
+    });
+    return results;
   };
 
   /*
@@ -164,6 +168,7 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+
   };
 
   // Determine if the array or object contains a given value (using `===`).
